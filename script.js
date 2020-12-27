7b20694ecad3469243d68c0d4d63669f2a988f26
arwhitus <arwhitus@gmail.com>
2020-12-26 23:31:20 -0500

    sensor read test

--- script.js
+++ script.js
@@ -10,21 +10,11 @@ function noDeviceSupport() {
 }
 
 function orientationUpdate(event) {
-    const beta = event.beta;  // In degree in the range [-180,180)
+    let beta = event.beta;  // In degree in the range [-180,180)
 
     if(beta == null) { noDeviceSupport(); }
 
     document.getElementById('pBeta').innerText = 'Beta = ' + beta;
-
-    // Because we don't want to have the device upside down
-    // We constrain the x value to the range [-90,90]
-    if (x >  90) { x =  90}
-    if (x < -90) { x = -90}
-
-    // To make computation easier we shift the range of
-    // x and y to [0,180]
-    x += 90;
-    y += 90;
 }
 
 if (window.DeviceOrientationEvent) {
